44294ac25d0198072114341782a4a86ada2566a6
suzukalight <mail@mkubara.com>
2019-10-27 23:53:47 +0900

    Add a11y addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { configure, addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+// import 'storybook-chromatic';
+
 import { GlobalStyle } from '../src/shared/global';
 
+addDecorator(withA11y);
 addDecorator(story => (
   <>
     <GlobalStyle />
@@ -9,5 +13,11 @@ addDecorator(story => (
   </>
 ));
 
-// automatically import all files ending in *.stories.js
-configure(require.context('../src', true, /\.stories\.js$/), module);
+// automatically import all files ending in *.stories.js|mdx
+configure(
+  [
+    require.context('../src', false, /Intro\.stories\.mdx/),
+    require.context('../src', true, /\.stories\.(js|mdx)$/),
+  ],
+  module,
+);
